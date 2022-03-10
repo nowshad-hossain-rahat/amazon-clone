f339a2018ecc466779f3dbd2de84467500078b1d
nowshad-hossain-rahat <nowshad.hossain.rahat@gmail.com>
2022-03-10 06:53:47 +0600

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDajmF20HCJco40MwvZFMu2gW5zbV5eM0Y",
+  authDomain: "clone-6539e.firebaseapp.com",
+  projectId: "clone-6539e",
+  storageBucket: "clone-6539e.appspot.com",
+  messagingSenderId: "328002027171",
+  appId: "1:328002027171:web:2566beb044402d3a92c916"
+};
