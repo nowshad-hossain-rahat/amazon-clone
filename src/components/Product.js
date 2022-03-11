fc3b23fda80a0c17da784fb9f461bead6089efa3
nowshad-hossain-rahat <nowshad.hossain.rahat@gmail.com>
2022-03-11 11:43:53 +0600

    made the products dynamic

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,31 +1,47 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product({id, title, price, rating, image}) {
+
+    let priceBeforeDot = parseFloat(price).toString().split('.')[0];
+    let priceAfterDot = parseFloat(price).toString().split('.')[1];
+
+    let stars = [];
+
+    for(let i = 1; i <= rating; i++){
+        stars.push(<span key={i}><i className="fa fa-star"></i></span>);
+    }
+
+    if(rating < 5){
+        for(let i = rating; i < 5; i++){
+            stars.push(<span key={i} className='negative'><i className="fa fa-star"></i></span>);
+        }
+    }
 
     return (
-        <div className="product">
+        <div className="product" key={id}>
             
             <div className="product-info">
 
-                <a className='product-title' href="#">Fiodio Mechanical Gaming Keyboard, LED Rainbow Gaming Backlit, 104 Anti-ghosting Keys, Quick-Response Black Switches, Multimedia Control for PC and Desktop Computer, with Removable Hand Rest</a>
+                <a className='product-title' href="#">
+                    {title.substr(0, 100) + '...'}
+                </a>
                 
                 <p className="product-price">
                     <small>$</small>    
-                    <strong>59<sup>99</sup></strong>
+                    <strong>
+                        {priceBeforeDot}
+                        <sup>{priceAfterDot}</sup>
+                    </strong>
                 </p>
 
                 <div className="product-rating">
-                    <span ><i className="fa fa-star"></i></span>
-                    <span ><i className="fa fa-star"></i></span>
-                    <span ><i className="fa fa-star"></i></span>
-                    <span ><i className="fa fa-star"></i></span>
-                    <span className='negative'><i className="fa fa-star"></i></span>
+                    {stars}
                 </div>
 
             </div>
 
-            <img src="keyboard.jpg" alt="Product Image" className="product-image" />
+            <img src={image} alt="Product Image" className="product-image" />
 
             <button className="add-to-basket-btn">Add to basket</button>
 
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,19 +6,48 @@ function ProductList(){
         <>
 
             <div className="home-row">
-                <Product />
-                <Product />
+                <Product 
+                    id={1}
+                    title={'Fiodio Mechanical Gaming Keyboard, LED Rainbow Gaming Backlit, 104 Anti-ghosting Keys, Quick-Response Black Switches, Multimedia Control for PC and Desktop Computer, with Removable Hand Rest'}
+                    price={59.99}
+                    rating={5} 
+                    image={'keyboard.jpg'}
+                />
+                <Product 
+                    id={2}
+                    title={'LG OLED C1 Series 65” Alexa Built-in 4k Smart TV (3840 x 2160), 120Hz Refresh Rate, AI-Powered 4K, Dolby Cinema, WiSA Ready, Gaming Mode (OLED65C1PUB, 2021)'}
+                    price={159.49}
+                    rating={4} 
+                    image={'tv.jpg'}
+                />
             </div>
 
             <div className="home-row">
-                <Product />
-                <Product />
-                <Product />
+                <Product 
+                    id={3} 
+                    title={'Logitech G502 HERO High Performance Wired Gaming Mouse, HERO 25K Sensor, 25,600 DPI, RGB, Adjustable Weights, 11 Programmable Buttons, On-Board Memory, PC / Mac'} 
+                    price={27.99} 
+                    image={'mouse.jpg'} 
+                    rating={4}
+                />
+                <Product 
+                    id={4} 
+                    title={'Seagate Portable 2TB External Hard Drive Portable HDD – USB 3.0 for PC, Mac, PlayStation, & Xbox - 1-Year Rescue Service (STGX2000400)'} 
+                    price={59.99} 
+                    image={'hdd.jpg'} 
+                    rating={5}
+                />
+                <Product 
+                    id={5} 
+                    title={'Seagate Portable 2TB External Hard Drive Portable HDD – USB 3.0 for PC, Mac, PlayStation, & Xbox - 1-Year Rescue Service (STGX2000400)'} 
+                    price={59.99} 
+                    image={'hdd.jpg'} 
+                    rating={4}
+                />
             </div>
             
             <div className="home-row">
-                {/* Product 1 */}
-                {/* Product 2 */}
+                {/* <Product /> */}
             </div>
 
         </>
